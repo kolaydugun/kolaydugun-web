343625f77de812f6f430ef1e24144c4ad9a77ff9
KolayDugun Dev <dev@kolaydugun.de>
2025-12-12 22:04:05 +0100

    datenshutz

--- src/App.jsx
+++ src/App.jsx
@@ -132,6 +132,7 @@ function App() {
                         <Route path="/blog" element={<Blog />} />
                         <Route path="/blog/:slug" element={<BlogPost />} />
                         <Route path="/faq" element={<FAQPage />} />
+                        <Route path="/p/:slug" element={<DynamicPage />} />
                         <Route path="/contact" element={<LeadForm />} />
                         <Route path="/weather" element={<Weather />} />
                         <Route path="/:vendorSlug/website" element={<PublicWedding />} />
